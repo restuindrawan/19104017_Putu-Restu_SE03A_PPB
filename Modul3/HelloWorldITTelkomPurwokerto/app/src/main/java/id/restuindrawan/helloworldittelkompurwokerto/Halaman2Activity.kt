95274a284a9aa389e7d07fc02dc08cb6f73cc890
Restu Indrawan <57904667+restuindrawan@users.noreply.github.com>
2022-02-03 20:09:15 +0800

    Add Modul3

---
+++ Modul3/HelloWorldITTelkomPurwokerto/app/src/main/java/id/restuindrawan/helloworldittelkompurwokerto/Halaman2Activity.kt
@@ -0,0 +1,12 @@
+package id.restuindrawan.helloworldittelkompurwokerto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rifqi_19104031.helloworldittelkompurwokerto.R
+
+class Halaman2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman2)
+    }
+}
